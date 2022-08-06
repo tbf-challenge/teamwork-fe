6ec76b83c043add53fe386dde145dcfe6a56036f
roji-tech <rojitech9@gmail.com>
2022-08-07 00:07:13 +0100

    added

--- src/components/button/ButtonStyle.js
+++ src/components/button/ButtonStyle.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
-const ButtonStyle = styled.button``;
+const ButtonStyle = styled.button`
+  background-color: var(--btn);
+`;
 
 export default ButtonStyle;
