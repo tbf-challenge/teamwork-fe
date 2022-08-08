e5a36a135a410562ed770f08df4acf4278e5263b
izima01 <kingsleyizima@gmail.com>
2022-08-08 14:13:15 +0100

    feat: fixed the responsiveness on different screens and icons on home screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,27 @@
+import { useRef } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import SideBar from "../components/sidebar/SideBar";
 import PagesStyle from "./pagesStyle";
 
 const PagesIndex = () => {
+  // const [showNav, setShowNav] = useState(false);
+  const navRef = useRef();
+  const ham = useRef();
+
+  const toggleNav = () => {
+    navRef.current.classList.toggle("toggleOn");
+    console.log(ham.current);
+    ham.current.classList.toggle("rotateOn");
+  };
   return (
     <PagesStyle>
-      <Navbar />
+      <Navbar
+        toggleNav={toggleNav}
+        hamRef={ham}
+      />
       <div className="pages">
-        <div className="sidebar">
+        <div className="sidebar" ref={navRef}>
           <SideBar />
         </div>
         <div className="outlet">
