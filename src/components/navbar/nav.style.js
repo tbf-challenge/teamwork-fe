d91c897fd4ac5b7d6db0527c3a7d8dcaca0f8cae
roji-tech <rojitech9@gmail.com>
2022-08-08 17:23:18 +0100

    _

--- src/components/navbar/nav.style.js
+++ src/components/navbar/nav.style.js
@@ -141,7 +141,7 @@ const NavStyle = styled.div`
     }
   }
 
-  @media (max-width: 500px) {
+  @media (max-width: 600px) {
     grid-template-columns: 12% 88%;
     border-bottom: 1px solid black;
     height: 50px;
@@ -186,81 +186,6 @@ const NavStyle = styled.div`
     #ham {
       display: block !important;
       transition: 1s;
-    }
-  }
-
-  @media screen and (max-width: 1025px) {
-  }
-
-  @media screen and (max-width: 800px) {
-    justify-content: space-around;
-    align-items: center;
-    height: 70px;
-    width: 100%;
-    display: flex;
-
-    .logoWrap {
-      width: max-content;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: #fff;
-      padding: 1px 10px;
-
-      h1.brand-name {
-        display: none;
-      }
-    }
-
-    .headWrap {
-      display: flex;
-      align-items: center;
-      padding-left: 5px;
-      width: 90%;
-      height: 100%;
-
-      .head {
-        border: 0;
-        width: 98%;
-        height: 70%;
-        align-items: center;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        border-radius: 4px;
-        padding: 5px;
-
-        .title {
-          display: none;
-        }
-
-        .search {
-          width: 40%;
-          height: 100%;
-          display: flex;
-          border-radius: 2px;
-          border: 0.9px solid rgba(0, 0, 0, 0.15);
-
-          .searchInput {
-            box-sizing: border-box;
-            width: 100%;
-            height: 100%;
-            outline: none;
-            padding: 5px 15px;
-
-            ::placeholder {
-              /* for styling placeholder */
-              color: red;
-              /* padding-left: 5px; */
-            }
-          }
-        }
-
-        .icons {
-          display: flex;
-        }
-      }
     }
   }
 `;
