17b31a1124ba93e084a48fcc5eb5705e97c25e0d
tayoski7 <tayoski7@gmail.com>
2022-08-10 00:41:51 +0100

    Employee Create Template Edited

--- src/pages/employees/Employees.jsx
+++ src/pages/employees/Employees.jsx
@@ -1,21 +1,19 @@
-import {
-  EmployeesContainer, 
-  EmployeesWrapper
-} from "./employees.style";
+import react from 'react'
+import {EmployeesContainer, EmployeesWrapper} from "./employees.style";
 import {useState} from 'react'
 
 const Employees = () => {
 
   const initialData = [
     {
-      firstName :'',
-      lastName:'',
-      email:'',
-      password:'',
-      jobRole:'',
-      department:'',
-      address:'',
-      gender:'',
+      firstName :"",
+      lastName:"",
+      email:"",
+      password:"",
+      jobRole:"",
+      department:"",
+      address:"",
+      gender:"",
     }
   ];
 
@@ -24,9 +22,9 @@ const Employees = () => {
   const handleChange =(e)=>{
     const {name,value,type,checked}= e.target;
     setUserData({...userData,[name]
-        :type ==='checkbox'
-        ?checked
-        :value});
+        : type ==="checkbox"
+        ? checked
+        : value});
   };
 
   return (
@@ -35,23 +33,25 @@ const Employees = () => {
       <EmployeesWrapper>
         <form>
 
-          <div className='rowDiv'>
-            <div className='inputDiv'>
-              <label >First Name</label><br/>
+          <div className="rowDiv">
+            <div className="inputDiv">
+              <label for="firstName">First Name</label>
+              <br/>
                 <input
-                  type={'text'} name={'firstName'}
-                  className='inputBox' required
+                  type={"text"} name={"firstName"}
+                  className="inputBox" required
                   value={userData.firstName}
                   onChange={handleChange}
                   minLength={3}
                 />
             </div>
 
-            <div className='inputDiv'>
-              <label >Last Name</label><br/>
+            <div className="inputDiv">
+              <label for="lastName">Last Name</label>
+              <br/>
                 <input
-                  type={'text'} name={'lastName'}
-                  className='inputBox' required
+                  type={"text"} name={"lastName"}
+                  className="inputBox" required
                   onChange={handleChange}
                   value={userData.lastName}
                   minLength={3}
@@ -59,102 +59,107 @@ const Employees = () => {
             </div>
           </div>
 
-          <div className='rowDiv'>
-            <div className='inputDiv'>
-              <label >Email</label><br/>
+          <div className="rowDiv">
+            <div className="inputDiv">
+              <label for="email">Email</label>
+              <br/>
                 <input
-                  type={'email'} name={'email'}
-                  className='inputBox' required
+                  type={"email"} name={"email"}
+                  className="inputBox" required
                   value={userData.email}
                   onChange={handleChange}
                 />
             </div>
 
-            <div className='inputDiv'>
-              <label >Password</label><br/>
+            <div className="inputDiv">
+              <label for="password">Password</label>
+              <br/>
                 <input
-                  type={'password'} name={'password'}
-                  className='inputBox' required
+                  type={"password"} name={"password"}
+                  className="inputBox" required
                   value={userData.password}
                   onChange={handleChange}
                   minLength={8}
                   // pattern='[a-zA-z0-9]{8}' 
-                  title='password must contain lower case,
-                        uppercase, numbers and special characters'
+                  title="password must contain lower case,
+                        uppercase, numbers and special characters"
                   />
             </div>
           </div>
 
-          <div className='radioDiv'>
+          <div className="radioDiv">
             <label>Gender</label>
-            <div className='radio'>
+            <div className="radio">
               <input
-                type={'radio'}
-                name={'gender'}
-                value={'male'} 
+                type={"radio"}
+                name={"gender"}
+                value={"male"} 
                 onChange={handleChange}
               />
-              <label 
-                className='radioInput'
+              <label for="male"
+                className="radioInput"
               >
                 Male
               </label>
               <input 
-                type={'radio'}  
-                name={'gender'}
-                value={'female'}
+                type={"radio"}  
+                name={"gender"}
+                value={"female"}
                 onChange={handleChange}
               />
-              <label
-                className='radioInput'
+              <label for="female"
+                className="radioInput"
               >
                 Female
               </label>
             </div>
           </div>
 
-          <div className='rowDiv'>
-            <div className='inputDiv'>
-              <label >Job Role</label><br/>
+          <div className="rowDiv">
+            <div className="inputDiv">
+              <label for="jobRole">Job Role</label>
+              <br/>
                 <input
-                  type={'text'} name={'jobRole'}
-                  className='inputBox'
+                  type={"text"} name={"jobRole"}
+                  className="inputBox"
                   value={userData.jobRole}
                   onChange={handleChange} 
                 />
             </div>
 
-            <div className='inputDiv'>
-              <label >Department</label><br/>
+            <div className="inputDiv">
+              <label for="department">Department</label>
+              <br/>
                 <input
-                  type={'text'} name={'department'}
-                  className='inputBox'
+                  type="text" name={"department"}
+                  className="inputBox"
                   value={userData.department}
                   onChange={handleChange}
                 />
             </div>
           </div>
 
-          <div className='addressDiv'>
-            <label >Address</label><br/>
+          <div className="addressDiv">
+            <label  for="address" >Address</label>
+            <br/>
               <input
-                type={'text'} name={'address'}
-                className='inputRegister'
+                type={"text"} name={"address"}
+                className="inputRegister"
                 value={userData.address}
                 onChange={handleChange} 
               />
           </div>
 
           <button
-            type={'submit'}
-            className='submitButton'
+            type={"submit"}
+            className="submitButton"
           >
             Create Account
           </button>
         </form>
       </EmployeesWrapper>
     </EmployeesContainer>
-  )
+  );
 };
 
 export default Employees;
