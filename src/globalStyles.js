df5055f3ba95038992da2d93280a716704c80386
yellow_may <yellow.may.21@gmail.com>
2022-08-29 13:53:45 +0100

    sign-in-mobile-ui

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-    :root{
-        width: 99.9%;
+    html{
+        height: 100%;
+    }
+
+    #root{
+        width: 100%;
+        height: 100%;
     }
     
     * {
@@ -13,13 +18,23 @@ const GlobalStyles = createGlobalStyle`
     }
 
     body {
-        width: 99.9%;
+        width: 100%;
+        height: 100%;
         box-sizing: border-box;
         /* background-color: gray; */
     }
     
     .App{
         box-sizing: border-box;
+        height: 100%;
+    }
+
+    :root {
+        --Text-Header-Color: #1F2937;
+        --Primary-Color: #1678F3;
+        --Pure-White: #FFFFFF;
+        --Gray-Color-300: #D2D5DA;
+        --Gray-Color-400: #9CA3AF;
     }
 `;
 
