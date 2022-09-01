39b598b982628642615f921da08ebdf002ecc142
Stephen <stephenadeyemo@gmail.com>
2022-09-01 04:25:23 +0100

    responsive categories page

--- src/pages/categories/CategoryTopbar.jsx
+++ src/pages/categories/CategoryTopbar.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
-import { Topbar } from "./categories.styles";
+import {
+  Topbar, Nav, Nav2, Nav3
+}
+  from "./categories.styles";
 import Forward from "../../Assets/images/forward.svg";
 
 const CategoryTopbar = () => {
   return (
     <Topbar>
-      <div className="topbar">
-        <ul>
-          <li><Link to="/categories">All</Link></li>
+      <Nav><Link to="/categories">All</Link></Nav>
+      <Nav2><Link to="/business">Business</Link></Nav2>
+      <Nav3><Link to="/event">Event</Link></Nav3>
+      {/* <Nav><Link to="/categories">Finance</Link></Nav>
+      <Nav><Link to="/categories">Technology</Link></Nav>
+      <Nav><Link to="/categories">UX</Link></Nav>
+      <Nav><Link to="/categories">Design</Link></Nav>
+      <Nav><Link to="/categories">Others</Link></Nav>
+      <Nav><Link to="/categories">Web3</Link></Nav> */}
+      <img src={Forward} alt="foward" />
+      {/* <div className="">
+        <ul className="links">
+          <li className="all"><Link to="/categories">All</Link></li>
           <li><Link to="/business">Business</Link></li>
           <li><Link to="/event">Event</Link></li>
           <li><Link to="/categories">Finance</Link></li>
@@ -16,9 +29,8 @@ const CategoryTopbar = () => {
           <li><Link to="/categories">Design</Link></li>
           <li><Link to="/categories">Others</Link></li>
           <li><Link to="/categories">Web3</Link></li>
-          <img src={Forward} alt="foward" />
         </ul>
-      </div>
+      </div> */}
     </Topbar>
   );
 };
