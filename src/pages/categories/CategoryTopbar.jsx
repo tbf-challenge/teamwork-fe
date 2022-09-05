38e42d1024010dffc6c3fa01a4d1c40fe073fe3d
roji-tech <rojitech9@gmail.com>
2022-09-05 06:47:29 +0100

    m

--- src/pages/categories/CategoryTopbar.jsx
+++ src/pages/categories/CategoryTopbar.jsx
@@ -28,20 +28,16 @@ const CategoryTopbar = () => {
 
   return (
     <Topbar>
-      <div className="topbar">
-        <ul>
-          <li><Link to="/categories">All</Link></li>
-          <li><Link to="/business">Business</Link></li>
-          <li><Link to="/event">Event</Link></li>
-          <li><Link to="/categories">Finance</Link></li>
-          <li><Link to="/categories">Technology</Link></li>
-          <li><Link to="/categories">UX</Link></li>
-          <li><Link to="/categories">Design</Link></li>
-          <li><Link to="/categories">Others</Link></li>
-          <li><Link to="/categories">Web3</Link></li>
-          <img src={Forward} alt="foward" />
-        </ul>
-      </div>
+      <li><Link to="/categories">All</Link></li>
+      <li><Link to="/business">Business</Link></li>
+      <li><Link to="/event">Event</Link></li>
+      <li><Link to="/categories">Finance</Link></li>
+      <li><Link to="/categories">Technology</Link></li>
+      <li><Link to="/categories">UX</Link></li>
+      <li><Link to="/categories">Design</Link></li>
+      <li><Link to="/categories">Others</Link></li>
+      <li><Link to="/categories">Web3</Link></li>
+      <img src={Forward} alt="foward" />
     </Topbar>
   );
 };
