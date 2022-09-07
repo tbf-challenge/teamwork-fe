824ec9bfc9400717127beebd4796ebe7f361e543
yellow_may <yellow.may.21@gmail.com>
2022-09-07 13:36:16 +0100

    reports page mobile view

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -1,7 +1,74 @@
-import ReportsStyles from "./reports.styles";
+import Styled from "./reports.styles";
+import { ReactComponent as BackSvg } from "../../Assets/images/Back.svg";
+import { ReactComponent as MoreSvg } from "../../Assets/images/more-vertical.svg";
+import userPic from "../../Assets/images/User Profile.png";
+
+const {
+  Wrapper, Heading, BackButton, Typography, Body, CardsContainer, Card
+} = Styled;
+
+const data = [
+  {
+    id: "0", user: { username: "@blessed", fullname: "Blessing F", profileImg: userPic }, reports: { amount: 2, type: "Harmful speech" }, date: "08/08/2022"
+  },
+  {
+    id: "1", user: { username: "@blessed", fullname: "Blessing F", profileImg: userPic }, reports: { amount: 4, type: "Harmful speech" }, date: "08/08/2022"
+  },
+  {
+    id: "2", user: { username: "@blessed", fullname: "Blessing F", profileImg: userPic }, reports: { amount: 1, type: "Harmful speech" }, date: "08/08/2022"
+  }
+];
 
 const Reports = () => {
-  return <ReportsStyles>Reports</ReportsStyles>;
+  return (
+    <Wrapper>
+      <Heading>
+        <BackButton type="button" title="back">
+          <BackSvg />
+        </BackButton>
+
+        <Typography.NavH1>Reports</Typography.NavH1>
+      </Heading>
+
+      <Body>
+        <CardsContainer>
+          {data.map(({
+            id, user, reports, date
+          }) => {
+            const items = [
+              { idx: "0", name: "Username", desc: user.username },
+              { idx: "1", name: "No. of Reports", desc: reports.amount },
+              { idx: "2", name: "Report type", desc: reports.type },
+              { idx: "3", name: "Date", desc: date }
+            ];
+
+            return (
+              <Card.Wrapper key={id}>
+                <Card.Heading>
+                  <div>
+                    <img src={user.profileImg} alt={user.username} />
+                  </div>
+                  <span>{user.fullname}</span>
+                  <button type="button" title="more">
+                    <MoreSvg />
+                  </button>
+                </Card.Heading>
+
+                <Card.Body>
+                  {items.map(({ idx, name, desc }) => (
+                    <Card.Item key={idx}>
+                      <h5>{name}</h5>
+                      <span>{desc}</span>
+                    </Card.Item>
+                  ))}
+                </Card.Body>
+              </Card.Wrapper>
+            );
+          })}
+        </CardsContainer>
+      </Body>
+    </Wrapper>
+  );
 };
 
 export default Reports;
--- src/pages/reports/reports.styles.js
+++ src/pages/reports/reports.styles.js
@@ -1,5 +1,108 @@
 import styled from "styled-components";
 
-const ReportsStyles = styled.section``;
+const ReportsStyles = {
+  Wrapper: styled.div`
+  font-family: var(--Base-Family)
+  `,
+
+  Heading: styled.div`
+  height: 50px;
+  border-bottom: thin solid var(--Gray-Color-300);
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 0 5%
+  `,
+
+  BackButton: styled.button`
+  outline: none;
+  background: none;
+  cusrsor: pointer;
+  `,
+
+  Typography: {
+    NavH1: styled.h1`
+    font-size: 0.9em;
+    margin-top: -4px;
+    `
+  },
+
+  Body: styled.div`
+  padding: 16px 12px
+  `,
+
+  CardsContainer: styled.div`
+  padding: 24px 6px;
+  border-radius: 8px;
+  border: thin solid var(--Gray-Color-200);
+  display: grid;
+  grid-template-column: 1fr;
+  gap: 32px;
+  `,
+
+  Card: {
+    Wrapper: styled.div`
+    border: thin solid var(--Gray-Color-200);
+    border-radius: 6px;
+    `,
+
+    Heading: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 24px 8px 8px 8px;
+    border-bottom: thin solid var(--Gray-Color-200);
+
+    > div {
+      width: 40px;
+      height: 40px;
+    };
+
+    > div > img {
+      width: 100%;
+    }
+
+    > span {
+      flex-grow: 1;
+      font-size: 1em;
+      font-weight: 700;
+      line-height: auto;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;    
+    };
+
+    > button {
+      outline: none;
+      background: none;
+      cursor: pointer;
+    }
+    `,
+
+    Body: styled.div`
+    padding: 24px 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    `,
+
+    Item: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h5 {
+      color: rgba(0,0,0, 0.8);
+      font-size: 0.98em;
+      font-weight: 400;
+    };
+
+    span {
+      font-size: 0.9em;
+      font-weight: 700;
+    }
+    `
+  }
+};
 
 export default ReportsStyles;
