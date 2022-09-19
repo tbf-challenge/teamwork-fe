bf77439674e8689003d2ebd78845ab4494f9d4d4
yellow_may <yellow.may.21@gmail.com>
2022-09-19 22:15:21 +0100

    reports page mobile view

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -1,15 +1,72 @@
+import { useState } from "react";
 import Styled from "./reports.styles";
 // import { ReactComponent as BackSvg } from "../../Assets/images/Back.svg";
 // import { ReactComponent as DeleteSvg } from "../../Assets/images/delete_outline.svg";
-// import userPic from "../../Assets/images/User Profile.png";
+import userPic from "../../Assets/images/User Profile.png";
+import articleImg from "../../Assets/images/article_1.png";
 
 const {
-  Wrapper
+  Wrapper, Header, Nav, NavButton, Main, Card
 } = Styled;
 
+const navGroup = [
+  { id: "0", label: "New", badge: 4 },
+  { id: "1", label: "In Progress", badge: 8 },
+  { id: "2", label: "Completed", badge: 26 }
+];
+
 const Reports = () => {
+  const [currNav, setCurrNav] = useState("New");
+
   return (
-    <Wrapper />
+    <Wrapper>
+      <Header>
+        <h1>Reports</h1>
+
+        <Nav>
+          {navGroup.map(({ id, label, badge }) => (
+            <NavButton key={id} type="button" active={label === currNav} onClick={() => setCurrNav(label)}>
+              {label}
+              &nbsp;
+              <span>{badge}</span>
+            </NavButton>
+          ))}
+        </Nav>
+      </Header>
+
+      <Main>
+        <Card.Wrapper to={`${1}`}>
+          <Card.Header>
+            <div className="img-wrapper">
+              <img src={userPic} alt="" />
+            </div>
+            <h3>Solange Spencer</h3>
+            <span>@ASA</span>
+          </Card.Header>
+
+          <Card.Body>
+            <div className="post-content">
+              <h4>Why Should Anyone Be Led by You?</h4>
+              <p>We all know that leaders need vision and energy. But to be inspirational...</p>
+            </div>
+
+            <div className="post-img-wrapper">
+              <img src={articleImg} alt="" />
+            </div>
+          </Card.Body>
+
+          <Card.Footer color="Secondary">
+            <div className="badge">Report type</div>
+
+            <div className="reporters">
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </Card.Footer>
+        </Card.Wrapper>
+      </Main>
+    </Wrapper>
   );
 };
 
--- src/pages/reports/reports.styles.js
+++ src/pages/reports/reports.styles.js
@@ -1,7 +1,156 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const ReportsStyles = {
-  Wrapper: styled.div``
+  Wrapper: styled.div`
+  font-family: var(--Base-Family);
+  padding: 8px 4%;
+  font-size: 1rem;
+  `,
+
+  Header: styled.div`
+  width: 100%;
+  margin-bottom: 16px;
+
+  h1 {
+    font-size: 1.25em;
+    margin-bottom: 0.75rem;
+    padding-left: 5px;
+  };
+  `,
+
+  Nav: styled.nav`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: thin solid var(--Gray-Color-200);
+
+  @media(min-width: 578px) {
+    justify-content: flex-start;
+    gap: 16px;
+  }
+  `,
+
+  NavButton: styled.button`
+  outline: none;
+  background: none;
+  cursor: pointer;
+  padding: 0 8px 5px;
+  position: relative;
+  color: ${(props) => (props.active ? "var(--Pure-Black)" : "var(--Text-Body-Color)")};
+  display: flex;
+  align-items: center;
+  gap: 2.5px;
+  font-size: 0.9em;
+  transition: all 0.25s ease-in-out;
+
+  span {
+    font-size: 0.75em;
+    padding: 0.05rem 0.5rem;
+    border: thin solid;
+    border-radius: 2rem;
+  };
+
+  &:first-child span {
+    border-color: var(--Primary-Color-500);
+  };
+  &:nth-child(2) span {
+    border-color: var(--Info-Color-600);
+  };
+  &:last-child span {
+    border-color: var(--Success-Color-600);
+  };
+
+  &::after {
+    content: ${(props) => (props.active ? "''" : "none")};
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: var(--Gray-Color-700);
+  };
+  `,
+
+  Main: styled.div`
+  display: grid;
+  gap: 16px;
+  `,
+
+  Card: {
+    Wrapper: styled(Link)`
+    border: thin solid var(--Gray-Color-300);
+    border-radius: 12px;
+    padding: 24px 12px;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    text-decoration: none;
+    color: unset;
+    `,
+
+    Header: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    .img-wrapper {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+      }
+    };
+
+    span {
+      font-size: 0.8em;
+    }
+    `,
+
+    Body: styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 7fr 3fr;
+    gap: 10px;
+
+    .post-content {
+      h4 {
+        margin-bottom: 5px;
+      }
+
+      p {
+        font-size: 0.85em;
+        color: var(--Text-Body-Color);
+      }
+    }
+
+    .post-img-wrapper {
+      img {
+        width: 100%:
+      }
+    }
+    `,
+
+    Footer: styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .badge {
+      font-size: 0.9em;
+      font-weight: 700;
+      padding: 0.5rem 1rem;
+      border-radius: 0.25rem;
+      color: ${(props) => (`var(--${[props.color]}-Color-600)`)};
+      background-color: ${(props) => (`var(--${[props.color]}-Color-50)`)};
+    }
+    `
+  }
 };
 
 export default ReportsStyles;
