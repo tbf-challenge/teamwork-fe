600988da3e9a54c465b57463a82d76c6749e9208
yellow_may <yellow.may.21@gmail.com>
2022-09-30 14:35:46 +0100

    api-requests-setup

--- src/apis/requests.js
+++ src/apis/requests.js
@@ -2,6 +2,6 @@ import axiosInstance from "./axios-instance";
 
 const signIn = (data) => axiosInstance.post("/auth/signin", data);
 
-const registerEmployee = (data) => axiosInstance.post("/auth/create-user", data);
+const inviteEmployee = (data) => axiosInstance.post("/auth/create-user", data);
 
-export { signIn, registerEmployee };
+export { signIn, inviteEmployee };
